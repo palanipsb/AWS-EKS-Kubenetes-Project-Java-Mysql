7c62cd753a5323865cab704cb9f8b3ab7e196b8e
Palanivel <palanipsb@gmail.com>
2025-02-26 13:46:28 -0400

    Updated

--- Terraform-Code/output.tf
+++ Terraform-Code/output.tf
@@ -1,16 +1,16 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.bankapp.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.bankapp.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.bankapp_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.bankapp_subnet[*].id
 }
 
